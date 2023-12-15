53abacc8318419fdf8509aac1382f8e2958bc8fb
America M <america.develops@gmail.com>
2023-12-15 14:23:49 -0500

    Add Day 09, Challenge 02 code

---
+++ 2023/09/day09b.py
@@ -0,0 +1,63 @@
+# GLOBAL VARIABLES
+# ----------------
+valueHistories = list()
+valuePredictions = list()
+
+# FUNCTIONS
+# ---------
+def isAllZeroes(p_numList : list):
+    if len(p_numList) > 0:
+        return all(num == 0 for num in p_numList)
+    else:
+        return False
+
+def getDifferencesSequence(p_numList : list):
+    differencesSequence = [None] * (len(p_numList) - 1)
+    for index in range (0, len(differencesSequence)):
+        difference = p_numList[index + 1] - p_numList[index]
+        differencesSequence[index] = difference
+    #print('Differences for', p_numList, 'is', differencesSequence)
+    return differencesSequence
+
+def getAllDifferenceSequences(p_valueHistory : list):
+    differenceSequences = [p_valueHistory]
+    currentDifferences = getDifferencesSequence(p_valueHistory)
+    differenceSequences.append(currentDifferences)
+    while not isAllZeroes(currentDifferences):
+        currentDifferences = getDifferencesSequence(currentDifferences)
+        differenceSequences.append(currentDifferences)
+    return differenceSequences
+
+def predictPrecedingValue(p_valueHistory : list):
+    pass
+    return -1
+
+
+# MAIN PROGRAM
+# ------------
+# Extract file contents
+# ---------------------
+filePath = "input.txt"
+inputFile = open(filePath, 'r')
+lines = inputFile.readlines()
+inputFile.close()
+del inputFile
+# Extract number histories
+# ------------------------
+for line in lines:
+    line = line.strip()
+    line = line.split(' ')
+    valueHistory = [int(num) for num in line]
+    valueHistories.append(valueHistory)
+del valueHistory
+del line
+del lines
+# Predict preceding values
+# ------------------------
+sumOfExtrapolatedValues = 0
+for history in valueHistories:
+    extrapolatedValue = predictPrecedingValue(history)
+    valuePredictions.append(extrapolatedValue)
+    sumOfExtrapolatedValues += extrapolatedValue
+print('Value predictions:', valuePredictions)
+print('SUM OF EXTRAPOLATED VALUES:', sumOfExtrapolatedValues)
