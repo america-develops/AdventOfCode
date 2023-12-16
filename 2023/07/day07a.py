a784f7757847eaceba90664944785cdb7306b3d3
America M <america.develops@gmail.com>
2023-12-16 17:10:54 -0500

    Finish function to determine the type of hand from a 5-character string

--- 2023/07/day07a.py
+++ 2023/07/day07a.py
@@ -1,47 +1,84 @@
 # GLOBAL VARIABLES
 # ----------------
-# Cards in descending order of relative strength
+# Cards in ascending order of relative strength
 cards = (
-    'A', 'K', 'Q', 'J', 'T',
-    '9', '8', '7', '6', '5',
-    '4', '3', '2'
+    '2', '3', '4', '5', '6',
+    '7', '8', '9', 'T', 'J',
+    'Q', 'K', 'A'
 )
 handTypes = (
-    'Five of a kind',
-    'Four of a kind',
-    'Full house',
-    'Three of a kind',
-    'Two pair',
+    'High card',
     'One pair',
-    'High card'
+    'Two pair',
+    'Three of a kind',
+    'Full house',
+    'Four of a kind',
+    'Five of a kind'
 )
 
 # FUNCTION DEFINITIONS
 # --------------------
 def determineHandType(p_handString : str):
+    uniqueCardCounts = dict()
+    for card in p_handString:
+        if card in uniqueCardCounts:
+            uniqueCardCounts[card] += 1
+        else:
+            uniqueCardCounts[card] = 1
+    numUniqueCards = len(uniqueCardCounts)
     # Check if five of a kind
-    # Check if four of a kind
-    # Check if full house
-    # Check if three of a kind
-    # Check if two pair
+    if numUniqueCards == 1: return handTypes[6]
+    # Check if only 2 unique cards
+    elif numUniqueCards == 2:
+        cardCounts = uniqueCardCounts.values()
+        # Check if four of a kind
+        if 4 in cardCounts: return handTypes[5]
+        # Check if full house
+        elif 3 in cardCounts: return handTypes[4]
+        # Invalid hand
+        else: return None
+    # Check if only 3 unique cards
+    elif len(uniqueCardCounts) == 3:
+        cardCounts = uniqueCardCounts.values()
+        # Check if three of a kind
+        if 3 in cardCounts: return handTypes[3]
+        # Check if two pair
+        elif 2 in cardCounts and sum(cardCounts) == 5:
+            return handTypes[2]
+        # Invalid hand
+        else: return None
     # Check if one pair
-    # Check if high card
-    pass
+    elif numUniqueCards == 4: return handTypes[1]
+    # Check if high card (all cards unique)
+    elif numUniqueCards == 5: return handTypes[0]
+    # Invalid hand
+    else: return None
 
-def isFiveOfAKind(p_handString : str):
-    pass
 
-def isFourOfAKind(p_handString : str):
-    pass
+# MAIN PROGRAM
+# ------------
+hands = list()
+hands.append('AAAAA')
+hands.append('AA8AA')
+hands.append('AAAA8')
+hands.append('8AAAA')
+hands.append('23332')
+hands.append('22333')
+hands.append('33322')
+hands.append('32323')
+hands.append('TTT98')
+hands.append('98TTT')
+hands.append('9TTT8')
+hands.append('T9T8T')
+hands.append('23432')
+hands.append('22433')
+hands.append('42233')
+hands.append('22334')
+hands.append('A23A4')
+hands.append('AA234')
+hands.append('234AA')
+hands.append('23AA4')
+hands.append('23456')
 
-def isFullHouse(p_handString : str):
-    pass
-
-def isThreeOfAKind(p_handString : str):
-    pass
-
-def isTwoPair(p_handString : str):
-    pass
-
-def isHighCard(p_handString : str):
-    pass
+for hand in hands:
+    print(hand, determineHandType(hand))
